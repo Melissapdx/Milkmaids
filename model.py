0c8b2ea4a33c8b734bd75cd40ccbb2a1e38d1f59
Melissa Orgill <orgillmelissa33@gmail.com>
2017-09-26 17:33:59 -0700

    Finished database model, working on repr

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
 """Model and database functions for Milkmaids"""
+from flask_sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy()
 
 
@@ -9,9 +11,13 @@ class Order(db.model):
 
     order_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    #order_status =
-    #order_date =
+    order_status = db.Column(db.String(30))
+    order_date = db.Column(db.DateTime)
 
+    def __repr__(self):
+        """Provide representation when printed"""
+
+        return "<Order order_id=%s user_id=%s order_status=%s order_date=%s>" %(self.order_id, self.user_id, self.order_status, self.order_date)
 
 class User(db.model):
     """Users of website"""
@@ -22,11 +28,15 @@ class User(db.model):
     firstname = db.Column(db.String(50), nullable=True)
     lastname = db.Column(db.String(50), nullable=True)
     username = db.Column(db.String(64), nullable=True)
-    password_encypt = db.Column(db.String(64), nullable=True)
+    password = db.Column(db.String(64), nullable=True)
     address = db.Column(db.String(64), nullable=True)
     zipcode = db.Column(db.Integer, nullable=True)
     email = db.Column(db.String(64), nullable=True)
 
+    def __repr__(self):
+        """Provide representation when printed"""
+
+        return "<User user_id=%s firstname=%s lastname=%s username=%s password=%s address=%s zipcode=%s email=%s>" %(self.user_id, self.firstname, self.lastname, self.username, self.password, self.address, self.zipcode, self.email)
 
 class Milk(db.model):
     """Milk for website"""
@@ -34,13 +44,17 @@ class Milk(db.model):
     __tablename__ = "milk"
 
     milk_id = db.Column(db.Integer, primary_key=True)
-    milk_diet = db.Column(db.String(30),)
-    #smoker =
+    smoker = db.Column(db.bool)
     baby_age = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     price_per_oz = db.Column(db.Integer)
-    #inventory =
+    inventory = db.Column(db.Integer)
+    date = db.Column(db.datetime)
 
+    def __repr__(self):
+        """Provide representation when printed"""
+
+        return "<Milk milk_id=%s smoker=%s baby_age=%s user_id=%s price_per_oz=%s inventory=%s date=%s>" %(self.milk_id, self.smoker, self.baby_age, self.user_id, self.price_per_oz, self.inventory, self.date)
 
 class Order_item(db.model):
     """Items in Order"""
@@ -52,15 +66,32 @@ class Order_item(db.model):
     milk_id = db.Column(db.Integer, db.ForeignKey('milk.milk_id'))
     quantity = db.Column(db.Integer)
 
+    def __repr__(self):
+        """Provide representation when printed"""
 
-class Milk_donation(db.model):
+        return "<Order_item>"
+class Milk_diet(db.model):
     """Milk donated to milk website"""
 
-    __tablename__ = "milk_donation"
+    __tablename__ = "milk_diet"
 
-    milk_donation_id = db.Column(db.Integer, primary_key= True)
-    diet_id = db.Column(db.String(30), db.ForeignKey('milk.milk_id'))
+    milk_diet_id = db.Column(db.Integer, primary_key=True)
+    milk_id = db.Column(db.Integer(30), db.ForeignKey('milk.milk_id'))
+    diet_id = db.Column(db.Integer(30), db.ForeignKey('diet.diet_id'))
 
+    def __repr__(self):
+        """Provide representation when printed"""
+
+
+class Diet(db.model):
+    """Stores diet information about milk"""
+
+    __tablename__ = "diet"
+    diet_id = db.Column(db.Integer, primary_key=True)
+    diet_name = db.Column(db.String(64))
+
+    def __repr__(self):
+        """Provide representation when printed"""
 
 
 #Helper functions
